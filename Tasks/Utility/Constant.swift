6660a0b761c73a736a611d5bfc1e739bb716deb6
NT <usedforfunny@gmail.com>
2022-04-21 10:20:43 +0200

    Code restructured, Added Constant file

---
+++ Tasks/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Tasks
+//
+//  Created by Alexandre Talatinian on 21/04/2022.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
