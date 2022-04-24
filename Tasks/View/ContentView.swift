be4afcba0e5c098b355ffed65e91881c4ff6c55b
NT <usedforfunny@gmail.com>
2022-04-24 18:43:18 +0200

    Importing Audio kit, adding sounds, and haptic feedback

--- Tasks/View/ContentView.swift
+++ Tasks/View/ContentView.swift
@@ -73,6 +73,8 @@ struct ContentView: View {
                         Button(action: {
                             // TOGGLE APPEARANCE
                             isDarkMode.toggle()
+                            playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         }, label: {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -91,6 +93,8 @@ struct ContentView: View {
                     
                     Button(action: {
                         showNewTaskItem = true
+                        playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
                     }, label: {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
