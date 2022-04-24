29361d375162dcda5987f0f5c25e3c14d5122a84
NT <usedforfunny@gmail.com>
2022-04-24 14:51:46 +0200

    Creating a custom blur effect when entry form is on

--- Tasks/View/BlankView.swift
+++ Tasks/View/BlankView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct BlankView: View {
     // MARK: - PROPERTIES
     
+    var backgroundColor: Color
+    var backgroundOpacity: Double
+    
     // MARK: - BODY
     
     
@@ -18,8 +21,9 @@ struct BlankView: View {
             Spacer()
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .background(Color.black)
-        .opacity(0.5)
+        .background(backgroundColor)
+        .opacity(backgroundOpacity)
+        .blendMode(.overlay)
         .edgesIgnoringSafeArea(.all)
     }
 }
@@ -29,6 +33,8 @@ struct BlankView: View {
 
 struct BlankView_Previews: PreviewProvider {
     static var previews: some View {
-        BlankView()
+        BlankView(backgroundColor: Color.black, backgroundOpacity: 0.3)
+            .background(BackgroundImageView())
+            .background(backgroundGradient.ignoresSafeArea(.all))
     }
 }
