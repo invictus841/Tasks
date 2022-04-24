306e79bc7ce986668299f6204abbf0776a458b90
NT <usedforfunny@gmail.com>
2022-04-24 14:36:24 +0200

    Improvement of the individual task item view
    creation of ListRowItemView and CheckboxStyle

---
+++ Tasks/View/ListRowItemView.swift
@@ -0,0 +1,33 @@
+//
+//  ListRowItemView.swift
+//  Tasks
+//
+//  Created by Alexandre Talatinian on 24/04/2022.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default)
+        } //: TOGGLE
+        .toggleStyle(CheckboxStyle())
+        .onReceive(item.objectWillChange, perform: { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        })
+    }
+}
+
+
